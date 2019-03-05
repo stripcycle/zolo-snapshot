b0d617f665157895ae31f796852c8f7b663638ef
@stripcycle <stripcycle@techno.email>
2019-03-05 12:08:43 -0800

    Probably a dead end, need a more reliable approach

--- import.js
+++ import.js
@@ -7,24 +7,19 @@ const async = require('async');
 const { execFile } = require('child_process');
 const scriptFile = process.argv[1];
 const appDir = path.dirname(scriptFile);
+const dataDir = path.resolve(appDir, 'data');
 
 process.chdir(appDir);
 
 const CWD = process.cwd();
-console.log(CWD);
-const argv = require('yargs').argv;
-let dataFile = argv._.shift();
-console.log(path.resolve(appDir, dataFile));
+// console.log(CWD);
+// console.log(path.resolve(appDir, dataFile));
 // process.exit();
 
-if (!fs.existsSync(path.resolve(appDir, dataFile))) {
-  console.error(`Missing dataFile: ${dataFile}`);
-  process.exit(1);
-}
-
-
-// const dataFile = path.resolve(CWD, argv._[0]);
-
+// if (!fs.existsSync(path.resolve(appDir, dataFile))) {
+//   console.error(`Missing dataFile: ${dataFile}`);
+//   process.exit(1);
+// }
 
 /*
  *  1. get raw json data files
@@ -37,11 +32,9 @@ function getDataFiles(dir) {
   });
 }
 
-function zipDataFiles(cb) {
-  let dir = path.resolve(appDir, 'data');
+function zipDataFiles(dir, cb) {
   let pExecFile = util.promisify(execFile);
   let dataFiles = getDataFiles(dir);
-  // console.log(dataFiles);
 
   let files = _.map(dataFiles, (file) => {
     return (cb) => {
@@ -60,29 +53,36 @@ function zipDataFiles(cb) {
   });
 }
 
-// console.log(getDataFiles('./data'));
-// zipDataFiles('./data', (e, r) => {
-//   process.exit();
-// });
-
-
-
-if (!fs.existsSync(dataFile)) {
-  console.log(`File ${dataFile} does not exist?`);
-  process.exit();
-}
-
-const data = require(dataFile).data;
-const headers = _.keys(data[0]);
-
-console.log(`Rows: ${data.length}`);
-
 const credentials = require('./Zolo-b45d3b3b2eb4.json');
 const sheetId = '1vrCahojYUqgkssi8HTFpTm8nTdboHGXEyBfxWFIBq2M';
 var doc = new GoogleSpreadsheet(sheetId);
 var sheet, sheets;
 
-function newSheet(step) {
+// function newSheet(step) {
+//
+// }
+//
+// // ** add a new worksheet to the document and import the data into it
+// function importData(step) {
+//
+// }
+
+function getInfo(step) {
+  doc.getInfo(function(err, info) {
+    if (err) throw err;
+    console.log('Loaded doc: '+info.title+' by '+info.author.email);
+    console.log(`Got ${info.worksheets.length} worksheets.`);
+    sheets = info.worksheets.slice(1); // retain one worksheet
+    let _sheet = info.worksheets[0]; // root sheet
+    console.log('sheet 1: '+_sheet.title+' '+_sheet.rowCount+'x'+_sheet.colCount);
+    step();
+  });
+}
+
+function importDataFromFile(file, callback) {
+  console.log('Importing file: %s', file);
+  let data = require(path.resolve(dataDir, file)).data;
+  let headers = _.keys(data[0]);
   let d = new Date();
   let sheetTitle = `${Date.now()} - ${d.toDateString()}`;
   var options = {
@@ -93,54 +93,63 @@ function newSheet(step) {
   doc.addWorksheet(options, (err, _sheet) => {
     if (err) throw err;
     sheet = _sheet; // set module global, hacky but it works.
-    step();
+    let _data = data;
+    _data = _data.slice(1);
+    sheet.setHeaderRow(headers, (err) => {
+      if (err) throw err;
+      let functions = _.map(_data, (row) => {
+        return (cb) => {
+          let _row = row;
+          _row.address = _.values(_row.address).join(" ");
+          sheet.addRow(_row, cb);
+        }
+      });
+
+      async.parallelLimit(functions, 20, (err, result) => {
+        if (err) throw err;
+        console.log('Done');
+        callback(result);
+      });
+    });
   });
 }
 
-function importData(step) {
-  let _data = data;
-  _data = _data.slice(1);
-  sheet.setHeaderRow(headers, (err) => {
-    if (err) throw err;
-    let functions = _.map(_data, (row) => {
-      return (cb) => {
-        let _row = row;
-        _row.address = _.values(_row.address).join(" ");
-        sheet.addRow(_row, cb);
-      }
-    });
+function main(dataDir, callback) {
+  let dataFiles = getDataFiles(dataDir);
+  // dataFiles.length = 1;
+  console.log('Found %d files to import.', dataFiles.length);
+  console.log(dataFiles);
 
-    async.parallelLimit(functions, 20, (err, result) => {
-      if (err) throw err;
-      console.log('Done');
-      step();
-    });
+  // callback();
+  let mainLoop = _.map(dataFiles, (file) => {
+    return (cb) => {
+      importDataFromFile(file, cb);
+    };
   });
 
-}
-
-async.series([
-  function setAuth(step) {
-    doc.useServiceAccountAuth(credentials, step);
-  },
-  function getInfo(step) {
-    doc.getInfo(function(err, info) {
-      if (err) throw err;
-      console.log('Loaded doc: '+info.title+' by '+info.author.email);
-      console.log(`Got ${info.worksheets.length} worksheets.`);
-      sheets = info.worksheets.slice(1); // retain one worksheet
-
-      // console.log(typeof step);
-      let _sheet = info.worksheets[0]; // root sheet
-      console.log('sheet 1: '+_sheet.title+' '+_sheet.rowCount+'x'+_sheet.colCount);
-      step();
-    });
-  },
-  newSheet,
-  importData,
-  zipDataFiles
-], function(err){
-    if( err ) {
-      console.log('Error: '+err);
+  async.series([
+    (step) => {
+      doc.useServiceAccountAuth(credentials, (err, r) => {
+        if (err) throw err;
+        step();
+      });
+    },
+    getInfo,
+    (step) => {
+      async.series(mainLoop, (err, result) => {
+        if (err) throw err;
+        callback(null, result);
+      });
     }
-});
+  ]);
+}
+
+if (require.main === module) {
+  console.log("dataDir: %s", dataDir);
+  main(dataDir, (err, result) => {
+    if (err) throw err;
+    // zipDataFiles(dataDir, () => {
+    //   console.log('Finished: %s', result);
+    // });
+  });
+}
