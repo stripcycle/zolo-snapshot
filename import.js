f2d57e79b148a1126612b0ea7148b3df8f64018b
@stripcycle <stripcycle@techno.email>
2019-03-18 12:16:05 -0700

    Implemented summarization of totals from all sheets into a table that can feed a timeline chart in the doc.

--- import.js
+++ import.js
@@ -11,12 +11,6 @@ process.chdir(appDir); // change to CWD
 
 const config = require('./config.json');
 
-/*
-TODO:
-
-1. calculate some basic stats and insert them into the same sheet
-*/
-
 const argv = require('yargs')
   .usage("Usage: $0 -m <test or prod> -f <datafile.json>")
   .alias('f', 'file')
@@ -44,8 +38,31 @@ const keyPath = path.resolve(appDir, 'Zolo-b45d3b3b2eb4.json');
 const creds = require(keyPath);
 const email = creds.client_email;
 const authClient = new google.auth.JWT(creds.client_email, keyPath, null, SCOPES);
-const sheets = google.sheets('v4');
+const sheetsApi = google.sheets('v4');
 
+/**
+  @param ts - a UNIX timestamp
+  @return a date formatted according to the string provided, or
+*/
+function formatDate(ts) {
+  let d = new Date(ts);
+  return `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()}`;
+  // return "Boo."
+}
+
+// get an array of sheet objects
+function getSheets(sheetId, callback) {
+  let req = {
+    auth: authClient,
+    spreadsheetId: sheetId
+  };
+  sheetsApi.spreadsheets.get(req, (err, result) => {
+    if (err) throw err;
+    callback(null, result.data.sheets);
+  });
+}
+
+// ceate a new sheet and import data into it.
 function createSheet(sheetId, info, data, callback) {
   info.gridProperties = {
     rowCount: data.length,
@@ -65,7 +82,7 @@ function createSheet(sheetId, info, data, callback) {
       ]
     }
   };
-  sheets.spreadsheets.batchUpdate(req, (err, result) => {
+  sheetsApi.spreadsheets.batchUpdate(req, (err, result) => {
     if (err) throw err;
 
     let range = info.title;
@@ -78,11 +95,74 @@ function createSheet(sheetId, info, data, callback) {
         values: data
       }
     };
-    sheets.spreadsheets.values.update(req, callback);
+    sheetsApi.spreadsheets.values.update(req, callback);
   });
 }
 
-// Authorize it to produce an access token
+// create a table of data in the first sheet with forumulae referring to
+// summary data from all subsequent sheets
+// use case is to feed a timeline chart showing summary fields change over time
+function setAnalysis(sheetId, callback) {
+  getSheets(sheetId, (err, sheets) => {
+    if (err) throw err;
+    let first = sheets.shift(); // remove first row
+    let arr = _.map(sheets, (sheet) => {
+      return sheet.properties.title;
+    });
+
+    let table = _.map(sheets, (sheet) => {
+      let sheetTitle = sheet.properties.title;
+      let _r = _.map(_.range(1, 5), (r) => {
+        return `='${sheetTitle}'!H${r}`;
+      });
+      // insert column A as a date column.
+      let ts = parseInt(sheetTitle.split(' - ').shift());
+      _r.unshift(formatDate(ts));
+      return _r;
+    });
+
+    // set headers
+    table.unshift(["Date", "Total listings", "Condos", "Townhouses", "Houses"]);
+
+    let strRange = `${first.properties.title}!A1:E${table.length}`;
+
+    let req = {
+      auth: authClient,
+      spreadsheetId: sheetId,
+      range: strRange,
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        majorDimension: "ROWS",
+        range: strRange,
+        values: table
+      }
+    };
+
+    sheetsApi.spreadsheets.values.update(req, callback);
+  });
+}
+
+function addSummary(sheetId, sheetTitle, callback) {
+  let strRange = `'${sheetTitle}'!G1:H4`;
+  let req = {
+    auth: authClient,
+    spreadsheetId: sheetId,
+    range: strRange,
+    valueInputOption: 'USER_ENTERED',
+    resource: {
+      range: strRange,
+      values: [
+        ["Total listings",  '=COUNT(A:A)'],
+        ["Condos",          '=COUNTIF(F:F, "condo")'],
+        ["Townhouses",      '=COUNTIF(F:F, "townhouse")'],
+        ["Houses",          '=COUNTIF(F:F, "house")'],
+      ],
+      majorDimension: "ROWS"
+    }
+  };
+  sheetsApi.spreadsheets.values.update(req, callback);
+}
+
 authClient.authorize(function(err, tokens) {
   if(err) throw err;
 
@@ -106,9 +186,27 @@ authClient.authorize(function(err, tokens) {
 
   rows.unshift(headers);
 
-  createSheet(sheetId, info, rows, (err, result) => {
+  let functions = [
+    (step) => { // create the new sheet and add data
+      createSheet(sheetId, info, rows, (err, result) => {
+        if (err) throw err;
+        step(null, result);
+      });
+    },
+    (step) => { // insert some summary fields with forumulae
+      addSummary(sheetId, info.title, (err, result) => {
+        if (err) throw err;
+        step(null, result);
+      });
+    },
+    (step) => {
+      setAnalysis(sheetId, step);
+    }
+  ];
+
+  async.series(functions, (err, result) => {
     if (err) throw err;
-    console.log(`${result.status} - ${result.statusText}`);
-    process.exit();
+    console.log(result);
   });
+
 });
